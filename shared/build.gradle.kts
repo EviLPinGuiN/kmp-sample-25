bdc162cd0d38b025e64ac355f59ede0fd9f30105
nailshaykhraziev <shaykhrazievnail@gmail.com>
2025-03-24 18:25:57 +0300

    Update toml.versions

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -29,12 +29,60 @@ kotlin {
         framework {
             baseName = "shared"
             isStatic = true
+            /*
+            * export
+            * случае экспорта модуля, для него генерятся нормальные obj-c хэдеры и со стороны айоса доступны все декларации,
+            * которые в нём объявлены. Без экспорта доступны только транзитивные штуки
+            * (например, если класс используется в shared или других экспортируемых модулях),
+            * имена будут с префиксом названия модуля, что-то типа FeatureNameSomeClassName
+            * */
         }
     }
-    
+
     sourceSets {
         commonMain.dependencies {
-            //put your multiplatform dependencies here
+            implementation(libs.bundles.ktorClientCommon)
+            implementation(libs.kotlinx.serialization.core)
+            implementation(libs.kotlinx.serialization.json)
+            implementation(libs.kotlinx.coroutines.core)
+            implementation(libs.kotlinx.datetime)
+
+            implementation(libs.multiplatform.settings)
+            implementation(libs.multiplatform.settings.serialization)
+
+            implementation(libs.sqldelight.coroutines.extensions)
+            implementation(libs.sqldelight.sqlite.adapter)
+
+            implementation(libs.koin.core)
+
+            implementation(libs.androidx.lifecycle.viewmodel)
+        }
+        androidMain.dependencies {
+            implementation(libs.ktor.client.okhttp)
+            implementation(libs.okhttp3.logging.interceptor)
+            implementation(libs.kotlinx.coroutines.android)
+
+            implementation(libs.sqldelight.android.driver)
+
+            implementation(libs.compose.ui)
+            implementation(libs.compose.ui.tooling.preview)
+            implementation(libs.compose.ui.tooling)
+            implementation(libs.compose.material3)
+            implementation(libs.compose.runtime)
+            implementation(libs.compose.foundation)
+
+            implementation(libs.androidx.lifecycle.runtime)
+            implementation(libs.androidx.lifecycle.runtime.compose)
+            implementation(libs.androidx.lifecycle.viewmodel.compose)
+
+            implementation(libs.coil)
+            implementation(libs.coilCompose)
+
+            implementation(libs.timber)
+        }
+        iosMain.dependencies {
+            implementation(libs.ktor.client.ios)
+            implementation(libs.sqldelight.native.driver)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
@@ -43,7 +91,7 @@ kotlin {
 }
 
 android {
-    namespace = "com.itis.examplekmp"
+    namespace = "com.itis.examplekmp.shared"
     compileSdk = 35
     defaultConfig {
         minSdk = 26
